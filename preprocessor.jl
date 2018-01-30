a08a91d305928f0d679e9f633057efe26fcb7ad3
Ludmilla Figueiredo <ludmilla.figueiredo@uni-wuerzburg.de>
2018-01-30 13:48:10 +0100

    Basic structure of the code the model - not in ecological sense, this is the structure of the scripts

---
+++ preprocessor.jl
@@ -0,0 +1,14 @@
+#
+# Ludmilla Figueiredo 2017
+#
+# Preprocessing and verification of model inputs
+
+#************************************************
+#					Inputs
+#************************************************
+# Read parameters
+# Read list of species and parameters
+# Read landscape files (GIS from R)
+## more on tests.jl
+#Initialize and populate landscape
+#----------------------
