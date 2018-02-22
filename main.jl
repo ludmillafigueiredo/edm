b12ceb0dc74f5de68431f7684b9c731fc2ae6a86
Ludmilla Figueiredo <ludmilla.figueiredo@uni-wuerzburg.de>
2018-02-22 14:11:24 +0100

    Proper call of modules and global variables (IDcounter defined in Organims and exported) and no methods errors with newOrgs!().

--- main.jl
+++ main.jl
@@ -9,14 +9,16 @@ push!(LOAD_PATH,EDDir)
 
 # Load modules:
 # General
-global GLOBAL_SEED =srand(123) # set seed for randomisation
+global ED_SEED =srand(123456789) # set seed for randomisation
 
-# World building
-using setworld
+# Load model packages
+using Distributions
+using Setworld
+using Organisms
 
-#TODO when reading for more files, considering transforming it in function to handle multiple entries
+#TODO when reading for more files, consider another input format to handle multiple entries: read as array might be enough
 begin
-    env = open("temp_prec.in")
+    env = open("landscape_init.in")
     readline(env); pxlength = parse(Int64,readline(env))
     readline(env); pylength = parse(Int64,readline(env))
     readline(env); pmeantemp = parse(Float64,readline(env))
@@ -26,9 +28,41 @@ begin
     readline(env); n_frags = parse(Int64,readline(env))
     close(env)
 end
-println("Environmental input read.")
+
+# read in which oganisms are going to be simulated
+# TODO stores it in some kind of
+begin
+    orgf = open("organisms.in")
+    readline(orgf); fgroups = [readline(orgf)] #doest need parse for string
+    readline(orgf); sps = [readline(orgf)]
+    readline(orgf); init_stage = [readline(orgf)]
+    readline(orgf); init_abund = [parse(Int64,readline(orgf))]
+    readline(orgf); genotypes = [readline(orgf)]
+    readline(orgf); biomassμ = [parse(Float64,readline(orgf))]
+    readline(orgf); biomasssd = [parse(Float64,readline(orgf))]
+    readline(orgf); dispμ = [parse.(Float64,readline(orgf))]
+    readline(orgf); dispsd = [parse.(Float64,readline(orgf))]
+    readline(orgf); radius = [parse(Int64,readline(orgf))]
+    close(orgf)
+end
+
+# Initialize individuals #TODO do proper simulation initialization
+# TODO worth keeping this function? just 3 lines
 
 mylandscape = landscape_init(false, pxlength, pylength, n_frags, pmeantemp, ptempsd, pmeanprec, pprecsd)
-
 println("Here is the landscape:")
-#TODO write("/mylandscape",mylandscape)
+write("/mylandscape",mylandscape)
+
+
+newOrgs!(mylandscape,
+fgroups,
+sps,
+init_stage,
+init_abund,
+biomassμ,
+biomasssd,
+genotypes,
+dispμ,
+dispsd,
+radius,
+IDcounter)
