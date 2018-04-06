0bbb3b8bf3bdcc28b78d7904db98e95e2d815126
Ludmilla Figueiredo <ludmillafi@gmail.com>
2018-04-06 10:52:23 +0200

    Better batch configuration: mem allocation,job id for output and error, stored in ED folder.

--- ED2plants.sh
+++ ED2plants.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #SBATCH -n 1 #number of cores
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=8G
+#SBATCH --workdir="/home/luf74xx/Dokumente/model/EDoutputs"
+#SBATCH --output=ED-%j.o
+#SBATCH --error=ED-%j.e
 
 /home/luf74xx/builds/julia-d386e40c17/bin/julia main.jl
