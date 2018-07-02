f00418a4af5d9f43b2c9e8e30a1e17247b1545c4
Ludmilla Figueiredo <ludmillafi@gmail.com>
2018-07-02 09:12:07 +0200

    Update gitignore with only model files

--- ED5y_spcspf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -n 12 #number of cores
-#SBATCH --mem-per-cpu=8G
-#SBATCH --output=ED-%j.o
-#SBATCH --error=ED-%j.e
-
-/home/luf74xx/builds/julia-d386e40c17/bin/julia main.jl --simID "singlesp1-5yspcspf" --landconfig "/home/luf74xx/Dokumente/model/inputs/landpars1m.csv" --spinput "/home/luf74xx/Dokumente/model/inputs/species.csv" --temp_ts "/home/luf74xx/Dokumente/model/inputs/envtimeseries_19995y.csv" --timesteps "260"
