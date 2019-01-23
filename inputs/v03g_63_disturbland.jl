2d048f84151cad02cfdb3f601e08fd7316c2ca76
Ludmilla Figueiredo <ludmillafi@gmail.com>
2019-01-23 19:36:50 +0100

    Test files for HPC.

---
+++ inputs/v03g_63_disturbland.jl
@@ -0,0 +1,4 @@
+fragsid = [1,2,3,4]
+fareas = [20000, 20000, 20000, 20000]
+disturbconnect = "/home/luf74xx/Dokumente/model/inputs/v03g_63_connect.csv"
+#disturbconnect = nothing #if single patch
---
+++ inputs/v03g_65_disturbland.jl
@@ -0,0 +1,4 @@
+fragsid = [1]
+fareas = [80000]
+#disturbconnect = "/home/luf74xx/Dokumente/model/inputs/initialconnect.csv"
+disturbconnect = nothing #if single patch
