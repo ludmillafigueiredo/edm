a4251efd1534b64ad1e3e0a03637a869574ff8f3
Ludmilla Figueiredo <ludmillafi@gmail.com>
2019-02-20 16:07:17 +0100

    Fix: Function to generate simulation commands
    Update for landscape disturbance files and replicating simulations.
    
    Related issues: #650

--- commandgen.R
+++ commandgen.R
@@ -1,6 +1,6 @@
-command <- function(simID, rseed, cluster, landf, connectsf, sppf, insectsf, disturb, areafile = NULL, duration, tout, tempf, outputat = "EDoutputs", timemsg = "false") {
+command <- function(cluster, EDdir, inputsdir, Juliadir, scriptsdir, simID, rseed, outputsdir, spinput, insectsf, initiallandf, disturbtype, landmode, disturbland, tdist, timesteps, tout, tempf, timemsg = "false",  reps = FALSE, nreps = NULL) {
   
-  # generate arguments to be parsed in Julia format 
+  # Generate arguments in Julia parse-format 
   parseformat <- function(x, y){
     if(missing(y)){
       paste('\"',ifelse(is.null(x),"",x),'\"', sep = "") #deal with single(not inside folder) or NULL arguments
@@ -9,18 +9,37 @@ command <- function(simID, rseed, cluster, landf, connectsf, sppf, insectsf, dis
     }
   }
   
+  # Set up directories
+  ## Julia and model folders change according to cluster
   if (cluster == "hpc") {
     # HPC directories
-    EDdir <- file.path("/home/ubuntu/", outputat)
-    inputsdir <- file.path("/home/ubuntu/Gaia/inputs")
-    Juliadir <- file.path("/home/ubuntu/builds/julia-9d11f62bcb/bin/julia")
+    if (missing(EDdir)){
+      EDdir <- file.path("/home/ubuntu/model")
+    }
+    if (missing(Juliadir)){
+      Juliadir <- file.path("/home/ubuntu/builds/julia-9d11f62bcb/bin/julia")
+    }
   } else {
     # Gaia directories
-    outputat <- file.path("EDoutputs")
-    EDdir <- file.path("/home/luf74xx/Dokumente/model", outputat)
-    inputsdir <- file.path(EDdir,"inputs")
+    if (missing(EDdir)){
+      EDdir <- file.path("/home/luf74xx/Dokumente/model")
+    }
+    
     Juliadir <- file.path("/home/luf74xx/builds/julia-d386e40c17/bin/julia")
   }
+  ## input/output directories do not depend on the cluster
+  if (missing(inputsdir)){
+    inputsdir <- file.path(EDdir, "inputs")
+  }
+  if (missing(outputsdir)){
+    outputsdir <- file.path(EDdir, "EDoutputs")
+  }
+  ## all scripts are written in Gaia, independently of where they are executed
+  if (missing(scriptsdir)){
+    scriptsdir <- file.path("/home/luf74xx/Dokumente/model/commandscripts")
+  }
+  
+  # Create command
   command <- paste(
     # julia folder and main
     Juliadir,
@@ -33,28 +52,25 @@ command <- function(simID, rseed, cluster, landf, connectsf, sppf, insectsf, dis
     parseformat(rseed),
     # outputat
     "--outputat",
-    parseformat(outputat),
-    "--landconfig",
-    # land file
-    parseformat(file.path(inputsdir,landf)),
-    "--connects",
-    # land file
-    parseformat(file.path(inputsdir,connectsf)),
+    parseformat(outputsdir),
     "--spinput", 
     # spp file
-    parseformat(file.path(inputsdir,paste(sppf))),
-    # insects file
+    parseformat(file.path(inputsdir,paste(spinput))),
     "--insect",
+    # insects file
     parseformat(file.path(inputsdir,insectsf)),
+    "--initialland",
+    # initial raster file
+    parseformat(file.path(inputsdir,initiallandf)),
+    "--disturbtype",
+    # land file
+    parseformat(disturbtype),
     # type of disturbance
-    "--disturb",
-    parseformat(disturb),
-    ## disturbance file
-    "--areafile",
-    parseformat(areafile), 
+    "--landmode",
+    parseformat(landmode),
     # sim duration
     "--timesteps",
-    parseformat(duration),
+    parseformat(timesteps),
     # output frequency
     "--tout",
     parseformat(tout),
@@ -64,10 +80,27 @@ command <- function(simID, rseed, cluster, landf, connectsf, sppf, insectsf, dis
     "--timemsg",
     parseformat(timemsg), 
     sep = " ")
+  ## non-obligatory arguments
+  if(!is.null(disturbland)){
+    command <- paste(command, 
+                     "--disturbland",
+                     ## disturbance file
+                     parseformat(file.path(inputsdir, disturbland)),
+                     sep = " ")
+  }
+  if (!is.null(tdist)){
+    command <- paste(command,
+                     "--tdist",
+                     ## time step of disturbance
+                     parseformat(file.path(inputsdir, tdist)),
+                     sep = " ")
+  }
+  
+  # Write command in script for safe-keeping 
   if (cluster == "gaia") {
     scriptname <- paste(simID,".sh", sep = "")
-    file.create(file.path(EDdir,scriptname))
-    bashscript <- file(file.path(EDdir,scriptname))
+    file.create(file.path(scripts,scriptname))
+    bashscript <- file(file.path(scripts,scriptname))
     writeLines(c("#!/bin/bash",
                  "#SBATCH -n 12 #number of cores",
                  "#SBATCH --mem-per-cpu=8G",
@@ -78,10 +111,66 @@ command <- function(simID, rseed, cluster, landf, connectsf, sppf, insectsf, dis
                bashscript)
   }else{
     scriptname <- paste(simID,"hpc.txt", sep = "")
-    file.create(file.path(file.path("/home/luf74xx/Dokumente/model"),scriptname )) # I create the scripts on Gaia, not in the HPC
-    hpcscript <- file(file.path(file.path("/home/luf74xx/Dokumente/model"),scriptname))
-    writeLines(command,hpcscript)
+    file.create(file.path(scriptsdir,scriptname)) # I create the scripts on Gaia, not in the HPC
+    hpcscript <- file(file.path(file.path(scriptsdir,scriptname)))
+    if (reps){
+      writeLines(c("!/bin/bash",
+                   paste("reps=($(seq 1 ", nreps, "))", sep = ""),
+                   paste("for i in `seq 1 ", nreps, "`; do", sep = ""),
+                   paste("simID=\"", simID, "_$i\"", sep = ""),
+                   paste("randomseed=\"$i\""),
+                   "",
+                   command),
+                 hpcscript)
+    }else{
+      writeLines(command,hpcscript)
     }
+  }
+  
+  
+  # bash file to run replicates
+  
+  #!/bin/bash
+  #reps=($(seq 1 29))
+  #for i in `seq 1 29`; do
+  #simID="v03g74_$i"
+  #randomseed="$i"
+  #/home/ubuntu/build/julia-9d11f62bcb/bin/julia main.jl --simID $simID --rseed $randomseed --outputat "/home/ubuntu/EDoutputs" --spinput "/home/ubuntu/model/inputs/v03g_66.csv" --insect "/home/ubuntu/model/inputs/insects.csv" --initialland "/home/ubuntu/model/inputs/v03g66initialland.jl" --disturbtype "frag" -landmode "artif" --tdist "/home/ubuntu/model/inputs/tdist52.csv" --timesteps "260" --landbuffer "/home/ubuntu/model/inputs/landbuffer66.jl" --tout "13" --temp <- ts "/home/ubuntu/model/inputs/temp1917_2017.csv" --timemsg "false" --disturbland "/home/ubuntu/model/inputs/v03g_74_disturbland.jl"
+  #done
+  
 }
 
-#command(simID = "testingcommandgen",rseed = 100,cluste="hpc",landf="whatever",sppf="whatever",insectsf="whatever",disturb="whatever",areafile = NULL,duration=10,tout=10,tempf="whatever",outputat = "EDoutputs", timemsg = "false")
+# Testing lines:
+
+# command(cluster = "gaia",
+#         simID = "testingcommandgen", 
+#         rseed = 100,
+#         spinput = "v03g_59.csv", 
+#         insectsf = "insects.csv", 
+#         initiallandf = "", 
+#         disturbtype = "none", 
+#         landmode = "artif", 
+#         disturbland = NULL, 
+#         tdist = NULL, 
+#         timesteps = 5200, 
+#         tout = 13, 
+#         tempf = "temp1917_2017.csv", 
+#         timemsg = "false",  
+#         reps = TRUE, 
+#         nreps = 30)
+# command(cluster = "hpc",
+#         simID = "testingcommandgen",
+#         rseed = 100,
+#         spinput = "v03g_59.csv",
+#         insectsf = "insects.csv",
+#         initiallandf = "",
+#         disturbtype = "none",
+#         landmode = "artif",
+#         disturbland = NULL,
+#         tdist = NULL,
+#         timesteps = 5200,
+#         tout = 13,
+#         timemsg = "false",
+#         tempf = "temp1917_2017.csv",
+#         reps = TRUE,
+#         nreps = 30)
