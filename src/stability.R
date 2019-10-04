3510b1bd8386b8d46dc56446d24a63464de38941
Ludmilla Figueiredo <ludmillafi@gmail.com>
2019-10-04 13:45:23 +0200

    Chore: Reorganized file/folder structure and names
    Cleaner and more intuitive

---
+++ src/stability.R
@@ -0,0 +1,16 @@
+#' Stability
+#' Implement stability measures from Hillebrand et al. (2018) for the results of the EDM module
+
+# Resistance
+## Transform orgsweekly into community matrix (from both the control and 
+## Get control community matrix
+
+## calculate
+braysim <- function(){}
+    ## get community 
+    fres <- log()
+    cres <-  #Bray Curtis because abundance-based
+    ## Resilience
+    ## Recovery
+    ## Temporal stability
+}
