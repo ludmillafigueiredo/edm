b19b3788cd539ef15a38918bcfc0b7b817367309
Ludmilla Figueiredo <ludmilla.figueiredo@uni-wuerzburg.de>
2019-10-10 18:21:13 +0200

    Refactor: Plant/Land related data structures go into entities module
    Related issues: #728

---
+++ src/entities.jl
@@ -0,0 +1,103 @@
+"""
+This module contains data structures used to represent entities in the model and to store values somehow related to them
+"""
+
+export SppRef, TraitRanges, Plant, LandPars, NeutralLandPars
+
+# Data structures and submodel functions related to the Plant entity
+# -----------------------------------------------------------------
+
+# Data structure holding individual trait values
+mutable struct Plant
+    id::String
+    stage::String #e,j,a
+    location::Tuple # (x,y)
+    sp::String #sp id, easier to read
+    kernel::String
+    clonality::Bool
+    #### Evolvable traits ####
+    seedmass::Float64
+    compartsize::Float64
+    span::Int64
+    firstflower::Int64
+    floron::Int64
+    floroff::Int64
+    seednumber::Int64
+    seedon::Int64
+    seedoff::Int64
+    bankduration::Int64
+    b0grow::Float64
+    b0germ::Float64
+    b0mort::Float64
+    #### State variables ####
+    age::Int64 # control death when older than max. lifespan
+    mass::Dict{String, Float64}
+    mated::Bool
+end
+
+# Initial trait values is read from an input file and stored for reference in `sppref::SppRef`.
+mutable struct SppRef
+    sp_id::Array{String, 1}
+    kernel::Dict{String,String}
+    clonality::Dict{String,Bool}
+    seedmass::Dict{String,Float64}
+    compartsize::Dict{String,Float64}
+    span_min::Dict{String,Float64}
+    span_max::Dict{String,Float64}
+    firstflower_min::Dict{String,Float64}
+    firstflower_max::Dict{String,Float64}
+    floron::Dict{String,Float64}
+    floroff::Dict{String,Float64}
+    seednumber_min::Dict{String,Float64}
+    seednumber_max::Dict{String,Float64}
+    seedon::Dict{String,Float64}
+    seedoff::Dict{String,Float64}
+    bankduration_min::Dict{String,Float64}
+    bankduration_max::Dict{String,Float64}
+    b0grow::Dict{String,Float64}
+    b0germ::Dict{String,Float64}
+    b0mort::Dict{String,Float64}
+    temp_opt::Dict{String,Float64}
+    temp_tol::Dict{String,Float64}
+    fitness::Dict{String, Float64}
+end
+
+# Minimal and maximal trait values, which control microevolution, are stored in `traitranges::TraitRanges`
+mutable struct TraitRanges
+    seedmass::Dict{String,Array{Float64,1}}
+    compartsize::Dict{String,Array{Float64,1}}
+    span::Dict{String,Array{Int64,1}}
+    firstflower::Dict{String,Array{Int64,1}}
+    floron::Dict{String,Array{Int64,1}}
+    floroff::Dict{String,Array{Int64,1}}
+    seednumber::Dict{String,Array{Int64,1}}
+    seedon::Dict{String,Array{Int64,1}}
+    seedoff::Dict{String,Array{Int64,1}}
+    bankduration::Dict{String,Array{Int64,1}}
+end
+
+# Data structures and submodel functions related to the Land entity
+# -----------------------------------------------------------------
+
+#Store simulation parameters referent to the landscape
+mutable struct LandPars
+    # Original landscape parameters
+    npatches::Int # patches of habitat (fragments are after disturbance)
+    plength::Array{Int64,1}
+    initialarea::Int64
+    initialconnect::Any # might not be simulated.
+    # Disturbed landscape parameters
+    nfrags::Any # might not be simulated
+    flength::Any # might not be simulated
+    disturbarea::Any # might not be simulated
+    disturbconnect::Any # might not be simulated
+    # general
+    bufferarea::Any # might not be simulated
+    meantempts::Array{Float64,1} # all fragments get the same temperature
+end
+
+mutable struct NeutralLandPars
+    initialland::Array{Int64,2}
+    disturbland
+    meantempts::Array{Float64,1}
+end
