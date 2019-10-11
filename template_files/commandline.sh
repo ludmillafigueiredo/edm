37e1cce1a5e4d2d2071b372d07122773d50f84eb
Ludmilla Figueiredo <ludmilla.figueiredo@uni-wuerzburg.de>
2019-10-11 10:18:14 +0200

    Style: Template of command line is more readable

--- template_files/commandline.sh
+++ template_files/commandline.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 # "simID" is the name the simulation should get
 
-/home/ubuntu/build/julia-9d11f62bcb/bin/julia src/run_EDM.jl --simID "simID" --rseed "100" --outputat "/home/ubuntu/model/EDoutputs" --spinput "/home/ubuntu/model/inputs/weisssingles/weissplants_sppinput.csv" --insect "/home/ubuntu/model/inputs/insects_indep.csv" --initialland "/home/ubuntu/model/inputs/control_4m2.grd" --disturbtype "none" --landmode "artif" --timesteps "5200" --tout "4" --temp_ts "/home/ubuntu/model/inputs/temp1917_2017.csv" --timemsg "true"
+/home/ubuntu/build/julia-9d11f62bcb/bin/julia src/run_EDM.jl\
+--simID "simID"\
+--rseed "100"\
+--outputat "EDoutputs"\
+--spinput "inputs/weisssingles/weissplants_sppinput.csv"\
+--insect "inputs/insects_indep.csv"\
+--initialland "inputs/control_4m2.grd"\
+--disturbtype "none"\
+--landmode "artif"\
+--timesteps "5200"\
+--tout "4"\
+--temp_ts "inputs/temp1917_2017.csv"\
+--timemsg "true"
