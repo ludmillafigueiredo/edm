ludmillafigueiredo/edm

commit af0b601d9126ec5704a4ccf67d1f26e49cb2ec5a

Fix: Annotation of disturbance
Tags of disturbance is more descriptive; vline added to all points timesteps of disturbance.

Related issues: #690

src/inputs.jl
@@ -43,7 +43,7 @@ function parse_commandline()
         default = joinpath("test_inputs","control_49m2.grd")
 
         "--disturb_type"
-        help = "Type of environmental disturbance to be implemented: habitat area loss \"loss\", habitat fragmentation \"frag\" or temperature change \"temp\""
+        help = "Type of environmental disturbance to be implemented: habitat area loss \"area_loss\", pollination loss \"poll_loss\", area and pollination loss \"area+poll_loss\" or temperature change \"temp\""
         arg_type = String
         default = "none"
 
@@ -75,12 +75,12 @@ read_landpars
 """
 function read_landpars(settings)
 	    
-	if settings["disturb_type"] == "loss"
+	if settings["disturb_type"] in ["area_loss", "area+poll_loss"]
                 disturbance = CSV.read(settings["disturb_file"], header = true,
 			      	       types = Dict("td" => Int64, "proportion" => Float64))
         elseif settings["disturb_type"] == "frag"
                 disturbance = CSV.read(settings["disturb_file"], header = true,
-			      	       types = Dict("td" => Int64, "disturb_file" => String))	    
+			      	       types = Dict("td" => Int64, "frag_file" => String))	    
         else
 		disturbance = nothing
 	end
@@ -168,11 +168,10 @@ Reads how insects are going to be implicitly simulated.
 """
 function read_pollination(settings::Dict{String,Any})
 
-    include(settings["pollination"])
-
-    if pollination_scen == "indep"
-        poll_pars = PollPars(pollination_scen, nothing)
+    if settings["disturb_type"] in ["poll_loss", "area+poll_loss"]
+        poll_pars = PollPars("indep", nothing)
     else
+       include(settings["pollination"])
        disturbed_regime= CSV.read(pollination_file, header = true,
 				  types = Dict("td" => Int64, "remaining" => Float64))
        poll_pars = PollPars(pollination_scen, disturbed_regime)	
@@ -191,7 +190,7 @@ function set_tdist(settings)
     # select file according to keyword: loss, frag, temp
         if settings["disturb_type"] == "none"
             tdist = nothing
-	elseif settings["disturb_type"] in ["loss" "frag"]
+	elseif settings["disturb_type"] in ["area_loss" "area+poll_loss"]
             tdist = CSV.read(settings["disturb_file"], header=true, types=Dict("td"=>Int64))[:td]
         elseif settings["disturb_type"] == "temp"
             println("Temperature change is simulated with the temperature file provided.")

src/analysED.R
@@ -16,11 +16,30 @@ library(cowplot);
 
 #### Directories and environment to store analysis ####
 #source("src/dirnames_setup.R")
+if(Sys.info()["user"] == "ludmilla"){
+  EDdir <- file.path("/home/ludmilla/Documents/uni_wuerzburg/phd_project/thesis/model/src")
+  EDdocsdir <- file.path(EDdir, "models_docs/EDODD")
+  traitsdir <- file.path("/home/ludmilla/Documents/uni_wuerzburg/phd_project/thesis","functional_types")
+}else if (Sys.info()["user"] == "ubuntu") {
+  EDdir <- file.path("/home/ubuntu/model/src")
+  traitsdir <- file.path(EDdir, "inputs")
+}else{
+  EDdir <- file.path("/home/luf74xx/model/src")
+  EDdocsdir <- file.path("/home/luf74xx/Dokumente/model_docs")
+  traitsdir <- file.path(EDdocsdir,"functional_types")
+}
+inputsdir <- file.path(EDdir, "test_inputs")
+if(nreps > 1 | missing(nreps)){
+  repfolder <- paste(file.path(outputsdir, parentsimID), 1:nreps, sep = "_"); #parentsimID
+}else{
+  repfolder <- outputsdir
+}
+
 analysEDdir <- file.path(outputsdir, paste(parentsimID, "analysED", sep = "_"))
 dir.create(analysEDdir)
 
 #### Theme, colours, labels ####
-source("src/theme_edm.R")
+source(file.path(EDdir,"theme_edm.R"))
 theme_set(theme_edm())
 size_labels = c('0.0001' = "Small",
                 '0.0003' = "Medium",
@@ -356,18 +375,18 @@ spprichness <- function(juvads_allreps_tab, pop_tab, parentsimID, disturbance,td
     geom_point(color = "darkslateblue", size = 1.25)+
     labs(x = "Time", y = "Species richness")
     
-    if (disturbance != "n"){
+    if (disturbance != "none"){
     
-        if (disturbance == "a"){
+        if (grepl(disturbanc == "area_loss")){
       	   text <- "Area loss"
-    	} else if (disturbance == "p"){
+    	} else if (disturbance == "poll_loss"){
       	   text <- "Pollination loss"
-    	} else if (disturbance == "ap"){
+    	} else if (disturbance == "area+poll_loss"){
       	   text <- "Area + pollination loss"
     	}
 
     spprichness_plot <- spprichness_plot +
-                        geom_vline(xintercept = tdist, linetype = 2, color = "red")
+                        geom_vline(xintercept = tdist, linetype = 2, color = "red", alpha = 0.5)
       
     }
   

src/outputs.jl
@@ -156,7 +156,7 @@ end
 analsyED()
 Run R script of analysis after the model finishes the simulation
 """
-function analysED(settings)
+function analysED(settings, land_pars, poll_pars)
 
     parentsimID = settings["simID"]
     @rput parentsimID
@@ -167,7 +167,16 @@ function analysED(settings)
     disturbance = settings["disturb_type"]
     @rput disturbance
 
-    outputsdir = results_folder
+    if settings["disturb_type"] in ["area_loss", "area+poll_loss"]
+        tdist = landpars.disturbance.td
+    elseif settings["disturb_type"]
+        tdist = poll_pars.regime.td
+    else
+	tdist = nothing
+    end
+    @rput tdist
+
+    Outputsdir = joinpath(settings["outputat"],settings["simID"])
     @rput outputsdir
 
     # run analysis

src/run_EDM.jl
@@ -16,7 +16,7 @@ include("unit_tests.jl")
 include("scheduling.jl")
 
 # run simulation
-results_folder = run_scheduling(settings, management_counter, landpars, poll_pars, K, T, mean_annual, plants, landscape)
+run_scheduling(settings, management_counter, landpars, poll_pars, K, T, mean_annual, plants, landscape)
 
 # analyse results
-analysED(settings)
+analysED(settings, landpars, poll_pars)

src/scheduling.jl
@@ -26,10 +26,9 @@ function run_scheduling(settings, management_counter, landpars, poll_pars, K, T,
 	    println("Species richness: $(length(unique(getfield.(plants, :sp))))")
 
             # APPLY LANDSCAPE DISTURBANCE
-	    if landpars.disturbance != nothing
-                if settings["disturb_type"] in ["frag" "loss"] && t in landpars.disturbance.td
-                    landscape = disturb!(landscape,plants,t,settings,landpars)
-                end
+	    if settings["disturb_type"] in ["frag", "area_loss", "area+poll_loss"] &&
+	       t in landpars.disturbance.td
+                landscape = disturb!(landscape,plants,t,settings,landpars)
 	    end
 	    
             updateK!(K, landscape, settings, t, landpars)
@@ -102,7 +101,4 @@ function run_scheduling(settings, management_counter, landpars, poll_pars, K, T,
 	    end
         end
     end
-
-    return settings, results_folder
-
 end

src/submodels_disturbances.jl
@@ -4,7 +4,7 @@ Change landscape structure and metrics according to the simulation scenario (`lo
 """
 function disturb!(landscape::BitArray{2}, plants::Array{Plant,1}, t::Int64, settings::Dict{String,Any}, landpars::LandPars)
 
-    if settings["disturb_type"] == "loss"
+    if settings["disturb_type"] in ["area_loss", "area+poll_loss"]
          landscape = destroyarea!(landpars, landscape, settings, t)
     elseif settings["disturb_type"] == "frag"
          landscape = load_fragmentation!(landpars, landscape, settings, t)

commit b580eb3fa841a7dd01ca889536c1d65a54a321d4

Fix: Storing of pollination parameter
Error introduced in af0b601d9126ec5704a4ccf67d1f26e49cb2ec5a.
Fixed before affecting simulations.

Related issues: #675

src/inputs.jl
@@ -168,13 +168,13 @@ Reads how insects are going to be implicitly simulated.
 """
 function read_pollination(settings::Dict{String,Any})
 
-    if settings["disturb_type"] in ["poll_loss", "area+poll_loss"]
-        poll_pars = PollPars("indep", nothing)
-    else
+    if occursin("poll", settings["disturb_type"]) 
        include(settings["pollination"])
        disturbed_regime= CSV.read(pollination_file, header = true,
 				  types = Dict("td" => Int64, "remaining" => Float64))
-       poll_pars = PollPars(pollination_scen, disturbed_regime)	
+       poll_pars = PollPars(pollination_scen, disturbed_regime)
+    else
+       poll_pars = PollPars("indep", nothing)
     end
     
     return poll_pars