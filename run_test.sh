f3e6e7d1ace75de31e6e2208c8efdf1d895f7118
ludmillfaigueiredo <ludmillafi@gmail.com>
2021-11-24 17:38:55 +0100

    Refactor: Non-version specific example of Julia path in example

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/your_path_to/julia-1.0.5/bin/julia src/run_EDM.jl --simID "test" --rseed "666" --sppinput "examples/test/sppinput_example.csv" --temp_ts "examples/test/temperature_example.csv" --initial_land "examples/test/4m2.grd" --disturb_type "none" --pollination "examples/test/pollination_example.jl" --output_freq "13" --outputat "."
+/your_path_to/julia-X.X.X/bin/julia src/run_EDM.jl --simID "test" --rseed "666" --sppinput "examples/test/sppinput_example.csv" --temp_ts "examples/test/temperature_example.csv" --initial_land "examples/test/4m2.grd" --disturb_type "none" --pollination "examples/test/pollination_example.jl" --output_freq "13" --outputat "."
